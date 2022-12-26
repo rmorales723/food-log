750dae7fee3ec7b3a7cb7af6af6dc2aae0ed8f04
robert morales <rmoralfit@gmail.com>
2022-12-26 15:35:57 -0500

    added categories

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
-
+    belongs_to :category
+    validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true
     def day
          self.created_at.strftime("%b %e, %Y")
     end
